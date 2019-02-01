2ff91258898af59aeaeeb88065e7a8121476ae46
krishnaw14 <krishnaw14@gmail.com>
2019-02-02 00:44:02 +0530

    U variation and Y variation codes

--- AE 312/Uvariation.py
+++ AE 312/Uvariation.py
@@ -4,11 +4,7 @@ import matplotlib.pyplot as plt
 import os 
 
 data_path = "2DJet/"
-corrected_data_path = "corrected_2DJet/"
 legends = []
-
-if not os.path.exists(corrected_data_path):
-	os.mkdir(corrected_data_path)
 
 station_distance_map = {
 	1:0,
@@ -19,13 +15,15 @@ station_distance_map = {
 	6:19,
 	7:22
 }
+x_values = [4.5, 8.5, 11.5, 16, 19, 22]
+U_plot_values = []
 
 for data_file_name in sorted(os.listdir(data_path)):
 
 	data = pd.read_csv(data_path + data_file_name)
 
-	# if data_file_name == "Sheet 1-Station 1.csv":
-	# 	continue
+	if data_file_name in ["Sheet 1-Station 1.csv"] :
+		continue
 
 	print(list(data))
 
@@ -33,7 +31,7 @@ for data_file_name in sorted(os.listdir(data_path)):
 	U_max = data.loc[data["U (m/s)"].idxmax()]["U (m/s)"]
 
 	U_values = abs(U_values - U_max/2)
-	# print(min(list(U_values)))
+
 	b_index = np.argmin(U_values)
 	b = abs(data["y (mm)"][b_index])
 	normalized_y = data["y (mm)"]/b
@@ -41,12 +39,9 @@ for data_file_name in sorted(os.listdir(data_path)):
 	del data['y/b']
 	data['y/y_0.5'] = pd.Series(normalized_y.astype(float).round(3), index=data.index)
 
-
-	plt.scatter(normalized_y, data["U/Um"], s = 8)
-
 	data["U/Um"] = data["U/Um"].astype(float).round(3)
-	# data.round({"U/Um": 3, "y/y_0.5": 3})
-	data.to_csv(corrected_data_path + data_file_name)
+
+	U_plot_values.append((U_max)**-2)
 
 	for i in range(1,8):
 		if "Station " + str(i) in data_file_name:
@@ -54,9 +49,15 @@ for data_file_name in sorted(os.listdir(data_path)):
 			legends.append(legend)
 			break
 
-plt.legend(legends)
-plt.xlabel("y/y_0.5")
-plt.ylabel("U/Um")
-plt.title("Nomalized Curve")
-plt.savefig("normalized_plot.png")
+plt.plot(x_values, U_plot_values, 'ro')
+m,c = np.polyfit(x_values, U_plot_values, 1)
+r = np.linspace(0,25,100)
+plt.plot(r, m*r + c)
+plt.legend(["Actual Data Points", "Approximate Linear fit function"])
+plt.xlabel("x/D")
+plt.ylabel("(U_0/U_M)")
+plt.xticks([])
+plt.yticks([])
+plt.title("Variation of Centerline Velocity with distance from the nozzle")
+plt.savefig("U.png")
 plt.show()
--- AE 312/Yvariation.py
+++ AE 312/Yvariation.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 import os 
 
 data_path = "2DJet/"
-corrected_data_path = "corrected_2DJet/"
 legends = []
 
-if not os.path.exists(corrected_data_path):
-	os.mkdir(corrected_data_path)
+# if not os.path.exists(corrected_data_path):
+# 	os.mkdir(corrected_data_path)
 
 station_distance_map = {
 	1:0,
@@ -19,13 +18,15 @@ station_distance_map = {
 	6:19,
 	7:22
 }
+x_values = [4.5, 8.5, 11.5, 16, 19, 22]
+b_values = []
 
 for data_file_name in sorted(os.listdir(data_path)):
 
 	data = pd.read_csv(data_path + data_file_name)
 
-	# if data_file_name == "Sheet 1-Station 1.csv":
-	# 	continue
+	if data_file_name in ["Sheet 1-Station 1.csv"] :
+		continue
 
 	print(list(data))
 
@@ -33,7 +34,7 @@ for data_file_name in sorted(os.listdir(data_path)):
 	U_max = data.loc[data["U (m/s)"].idxmax()]["U (m/s)"]
 
 	U_values = abs(U_values - U_max/2)
-	# print(min(list(U_values)))
+
 	b_index = np.argmin(U_values)
 	b = abs(data["y (mm)"][b_index])
 	normalized_y = data["y (mm)"]/b
@@ -42,11 +43,13 @@ for data_file_name in sorted(os.listdir(data_path)):
 	data['y/y_0.5'] = pd.Series(normalized_y.astype(float).round(3), index=data.index)
 
 
-	plt.scatter(normalized_y, data["U/Um"], s = 8)
+	# plt.scatter(normalized_y, data["U/Um"], s = 8)
 
 	data["U/Um"] = data["U/Um"].astype(float).round(3)
 	# data.round({"U/Um": 3, "y/y_0.5": 3})
-	data.to_csv(corrected_data_path + data_file_name)
+	# data.to_csv(corrected_data_path + data_file_name)
+
+	b_values.append(b/18)
 
 	for i in range(1,8):
 		if "Station " + str(i) in data_file_name:
@@ -54,9 +57,16 @@ for data_file_name in sorted(os.listdir(data_path)):
 			legends.append(legend)
 			break
 
-plt.legend(legends)
-plt.xlabel("y/y_0.5")
-plt.ylabel("U/Um")
-plt.title("Nomalized Curve")
-plt.savefig("normalized_plot.png")
+# print(legends)
+plt.plot(x_values, b_values, 'ro')
+m,c = np.polyfit(x_values, b_values, 1)
+r = np.linspace(0,25,100)
+plt.plot(r, m*r + c)
+plt.legend(["Actual Data Points", "Approximate Linear fit function"])
+plt.xlabel("x/D")
+plt.ylabel("b/D")
+plt.xticks([])
+plt.yticks([])
+plt.title("Variation of jet half width with distance from the nozzle")
+plt.savefig("Y.png")
 plt.show()
